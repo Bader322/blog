2e0c5b260eead7be50d76301eee7d1f9441fb30e
bader <bader322>
2020-01-28 16:22:59 -0800

    added about page navigation, corrected

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var path = require("path");
-// express package 
+const path = require('path');
+ 
 const express = require('express');
- // new instance of package
+ 
 const app = new express();
  
 app.use(express.static('public'));
  
-// Routes 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/views/index.html'));
+    res.sendFile(path.resolve(__dirname+ '/views/index.html'));
 });
 
-app.get('/views/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/views/about.html'));
+app.get('/index', (req, res) => {
+  res.sendFile(path.resolve(__dirname+ '/views/index.html'));
 });
 
-app.listen(4000, () => {
-    console.log('App listening on port 4000')
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname+ '/views/about.html'));
+});
+ 
+app.listen(3000, () => {
+    console.log('App listening on port 3000')
 });
