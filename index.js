9c730b3c6cc4f8153cec0caf8edd16e2f930cd29
bader <bader322>
2020-01-28 17:03:18 -0800

    fixed routing issue on homepage

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.get('/index', (req, res) => {
   res.sendFile(path.resolve(__dirname+ '/views/index.html'));
 });
 
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname+ '/views/contact.html'));
+});
+
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname+ '/views/post.html'));
+});
+
 app.get('/about', (req, res) => {
     res.sendFile(path.resolve(__dirname+ '/views/about.html'));
 });
