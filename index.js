7e5d0b7c4a1bebdd63553888951358f4f68d7629
bader <bader322>
2020-01-29 19:14:13 -0800

    about to try edge temp engine

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
+
+
 const path = require('path');
  
 const express = require('express');
  
-const app = new express();
+const app = express();
  
+const edge = require('edge.js')
+
 app.use(express.static('public'));
  
+
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname+ '/views/index.html'));
+  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 });
+
 
 app.get('/index', (req, res) => {
-  res.sendFile(path.resolve(__dirname+ '/views/index.html'));
+  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 });
 
+// Routes, nav bar links
+
+
+
+
+
+
 app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname+ '/views/contact.html'));
+  res.sendFile(path.resolve(__dirname+ '/pages/contact.html'));
 });
 
 app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname+ '/views/post.html'));
+  res.sendFile(path.resolve(__dirname+ '/pages/post.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname+ '/views/about.html'));
+    res.sendFile(path.resolve(__dirname+ '/pages/about.html'));
 });
  
 app.listen(3000, () => {
